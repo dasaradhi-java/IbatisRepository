f2b5d103c651c9f04e2fff1d0b945ae84ba88953
dasaradhi-java <dasaradhi.eluri@gmail.com>
2023-11-01 15:38:34 +0530

    first commit

---
+++ src/main/java/com/IBatisConfigurationThroughXml/IBatisConfigurationThroughXmlApplication.java
@@ -0,0 +1,18 @@
+package com.IBatisConfigurationThroughXml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class IBatisConfigurationThroughXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBatisConfigurationThroughXmlApplication.class, args);
+		
+		
+	}
+
+}
